e0b7ccfa0ff5e0aaab38de7acc8152e5e0923bca
Aaron McRuer <a.mcruer@live.com>
2017-04-03 21:08:05 -0500

    fails to comple because topnav is wrong?

---
+++ src/app/components/TopNav.js
@@ -0,0 +1,27 @@
+/**
+ * Created by aaronmcruer on 4/3/17.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+export default class TopNav extends React.Component {
+    render() {
+        return (
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Green Spaces</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Sign Up/ Log In</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -4,24 +4,15 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {Grid, Row, Col, ButtonGroup} from 'react-bootstrap';
+import  {TopNav}  from 'components/TopNav';
+
 class App extends React.Component {
 
     render() {
         return (
-            <Grid>
-                <Row>
-                    <Col xs={12} md={8}>icon</Col> <!-- the icon row -->
-                </Row>
-                <Row>
-                    <Col xs={12} md={8}>about</Col> <!-- the about button row -->
-                </Row>
-                <Row>
-                <Col xs={12} md={8}>
-
-                </Col> <!-- the login/sign-in row-->
-                </Row>
-            </Grid>
+            <TopNav/>
         )
     }
 }
+
+render(<App/>, window.document.getElementById('app'));
